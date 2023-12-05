68c02765c5f8002bbd85e0fb6b429fe20672e74c
tjennr <j.truong@yale.edu>
2023-12-04 22:53:07 -0500

    dont pull this

--- react/src/components/CardItem.js
+++ react/src/components/CardItem.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function CardItem(props) {
+// const DataDisplay = ({ dataFromFlask }) => {
+//   return (
+//     <div>
+//       {dataFromFlask.map((item, index) => (
+//         <div key={index}>
+//           {/* Access each dictionary item's properties here */}
+//           <p>Name: {item.name}</p>
+//           <p>Age: {item.age}</p>
+//           {/* Render other properties similarly */}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+function CardItem(trials) {
   return (
     <>
       <li className='cards__item'>
-        <Link className='cards__item__link' to={props.path}>
-          <figure className='cards__item__pic-wrap' data-category={props.label}>
+        <Link className='cards__item__link' to={trials.path}>
+          <figure className='cards__item__pic-wrap' data-category={trials.department}>
             <img
               className='cards__item__img'
               alt='Travel Image'
-              src={props.src}
+              src={trials.src}
             />
           </figure>
           <div className='cards__item__info'>
-            <h5 className='cards__item__text'>{props.text}</h5>
+            <h5 className='cards__item__text'>{trials.title}</h5>
           </div>
         </Link>
       </li>
